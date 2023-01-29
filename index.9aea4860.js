d5c5aa7db8fe388a1c298bea6a2d360a3fe7ba98
brzozanet <pawel@brzoza.net>
2023-01-29 22:18:20 +0000

    Deploying to gh-pages from @ brzozanet/goit-projekt-html-css-6@2600ec9f957648f399fe01001420b7d88ebbba21 🚀

---
+++ index.9aea4860.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-franschise-open]"),closeModalBtn:document.querySelector("[data-modal-franschise-close]"),modal:document.querySelector("[data-modal-franschise]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.9aea4860.js.map

c5bf71c92b137521c2b8ab49ff1ad8e5b54df5a2
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-01-31 16:02:07 +0100

    Mobile menu closing on 1280px fixed

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -16,7 +16,7 @@
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
